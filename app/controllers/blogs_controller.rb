4373b8434afd8f54348a5c18fd031eccd246bdcd
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-02-02 15:00:47 -0600

    new action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,12 @@ class BlogsController < ApplicationController
     def index
         @blogs = Blog.all
     end
+
+    def new
+        @blog = Blog.new
+    end
+
+    def create
+        # @blog = Blog.new
+    end
 end
