96915bc813b7ea461ac27dfe8bced78c7f14e903
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-02-03 11:39:54 -0600

    Update Action Complete

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,8 +16,11 @@ class BlogsController < ApplicationController
 
         # if blog was saved redirect to index 
         if @blog.save 
+            flash[:message] = "Blog was successfully saved!"
+            #if blog was saved redirect
             redirect_to blogs_path
         else 
+            #other wise rerender
             render :new, status: :unprocessable_entity
         end
         # else re-renmder new will be false if validates is not filled out
@@ -29,4 +32,25 @@ class BlogsController < ApplicationController
     def show 
         @blog = Blog.find(params[:id])
     end
+
+#====================================================================
+
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+
+#====================================================================
+
+    def update
+        @blog = Blog.find(params[:id])
+
+        if @blog.update(title: params[:blog][:title], description: params[:blog][:description])
+            flash[:message] = "Blog was successfully saved!"
+            redirect_to blog_path(@blog)
+        else 
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+
 end
